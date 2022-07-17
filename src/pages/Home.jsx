099856f01c729648daaf9f68e07df82280901db8
hr-meheraj <hrmeheraj2021@gmail.com>
2022-07-17 18:19:23 +0600

    footer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import HomeHeader from '../components/HomeHeader'
 import Products from '../components/Products'
 
@@ -7,6 +8,7 @@ export default function Home() {
         <>
        <HomeHeader/>
         <Products/>
+        <Footer/>
         </>
     )
 }
